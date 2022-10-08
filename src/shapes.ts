7c006d9d7201ce18b2c1a2183e9a64e91f82ae55
Bowen Xiao <z5416195@ad.unsw.edu.au>
2022-10-08 21:31:06 +1100

    finished

--- src/shapes.ts
+++ src/shapes.ts
@@ -1,73 +1,57 @@
 export function drawShape (shape: string, size: number, isSolid: boolean) {
-    // TODO: Fix the horrendous code below to be eslint-compliant.
-    // FIXME: Remove all TODOs and FIXMEs and you are done!
-    const TrIaNgLe_Shape = 'triangle';
-  const valid_shapes = [
-      TrIaNgLe_Shape,
-        'square'
-   ]
+  const triangleShape = 'triangle';
+  const validShapes = [
+    triangleShape,
+    'square',
+  ];
 
-
-    if (!valid_shapes.includes(shape)) return 'Invalid Input'
+  if (!validShapes.includes(shape)) return 'Invalid Input';
   if (size < 0) return 'Invalid Input';
 
-    let result = '' ;
-   if (shape==='square'&&isSolid) {
-        for (let row = 0; row < size; row++) {
-         let line= ''
-         for (let col = 0 ; col < size ; col++) {
-              line += '* '
-        }
-          result += line.trim() + '\n'
-       }
+  let result = '';
+  if (shape === 'square' && isSolid) {
+    for (let row = 0; row < size; row++) {
+      let line = '';
+      for (let col = 0; col < size; col++) {
+        line += '* ';
+      }
+      result += line.trim() + '\n';
     }
-     else
-      if ( shape === TrIaNgLe_Shape && isSolid === true ) {
-           for (let row = 0; row < size; row ++) {  
-           let line ='';          
-     for (let col = 0; col <= row; col ++) {
-           line += '* ';     
+  } else
+  if (shape === triangleShape && isSolid === true) {
+    for (let row = 0; row < size; row++) {
+      let line = '';
+      for (let col = 0; col <= row; col++) {
+        line += '* ';
+      }
+      result += line.trim() + '\n';
+    }
+  } else if (shape === 'square' && !isSolid) {
+    for (let row = 0; row < size; row++) {
+      let line = '';
+      for (let col = 0; col < size; col++) {
+        if (row === 0 || row === size - 1 || col === 0 || col === size - 1) {
+          line += '* ';
+        } else {
+          line += '  ';
         }
-            result += line.trim() + "\n";  
-            }
-   } else if (shape =='square' && !isSolid) {
-        for (let row = 0; row < size; row++) {
-           let line = ``;
-           for (let col = 0; col < size; col++) {
-               if (row === 0 || row === size - 1 || col === 0 || col === size - 1) {
-                    line += '* '
-               } else {
-                   line += '  ';
-               }
-           }
-            let undefinedVariable;
-           console.log(undefinedVariable);
-           result += line.trim() + '\n';
-       }
-    } else if (shape == `triangle` && isSolid===false) {
-       console.log("I'm a debugging print statement, remove me >:C!");
-       for (let row = 0; row < size; row++)
-       {
-            let line = '';
-          for (let col=0; col<=row;col++)
-           {
-                if (row === 0 || row === size-1 || col === 0 || col === row)
-             {
-                    line += '* ';
-            }
-                else
-               {
-                   line += '  ';
-               }
-           }
-           result += `${line}`.trim() + '\n';
-       }
-   } else {
-
-        const importantVariable = 'Why are we still here, just to suffer'
-       result+='Invalid Input'
-   }
-    // The line below is indented with a tab character (\t), not 4 spaces
-return result.trim();
-
+      }
+      result += line.trim() + '\n';
+    }
+  } else if (shape === 'triangle' && isSolid === false) {
+    for (let row = 0; row < size; row++) {
+      let line = '';
+      for (let col = 0; col <= row; col++) {
+        if (row === 0 || row === size - 1 || col === 0 || col === row) {
+          line += '* ';
+        } else {
+          line += '  ';
+        }
+      }
+      result += `${line}`.trim() + '\n';
+    }
+  } else {
+    result += 'Invalid Input';
+  }
+  return result.trim();
 }
